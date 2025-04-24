3879d01581e817642db2ea9a45c74ce3373ffe00
John Kono <u06jk21@abdn.ac.uk>
2025-04-24 02:18:42 +0100

    updates

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-//used to resolve the path to the static files
+
 const path = require('path');
 
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-// Static files from 'www' folder will be served
+
 app.use(express.static(path.join(__dirname, 'www')));
 
-// Default route to serve the index.html file
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'www', 'index.html'));
 });
